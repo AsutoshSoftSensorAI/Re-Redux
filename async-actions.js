76f579ac78308ae84692aea1a0673a8de737386a
AsutoshSoftSensorAI <asutosh.acharya@softsensor.ai>
2023-01-31 21:05:27 +0530

    initial commit

---
+++ async-actions.js
@@ -0,0 +1,84 @@
+const redux = require("redux");
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const initial_state = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
+
+const reducer = (state = initial_state, actions) => {
+  switch (actions.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: actions.payload,
+        error: "",
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: actions.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const users = res.data.map((user) => {
+          return user.username;
+        });
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((err) => {
+        dispatch(fetchUsersFailure(err.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(fetchUsers());
